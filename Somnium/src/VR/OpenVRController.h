714230a5e300d6be743a3b991f7ad72771cd4035
Luke Rose <37333663+MrLukeKR@users.noreply.github.com>
2023-09-09 16:27:12 +0100

    Added OpenVR SDK 1.26.7

---
+++ Somnium/src/VR/OpenVRController.h
@@ -0,0 +1,4 @@
+#pragma once
+#include "../../Dependencies/openvr/include/openvr.h"
+#include "../../Dependencies/openvr/include/openvr_capi.h"
+#include "../../Dependencies/openvr/include/openvr_driver.h"
